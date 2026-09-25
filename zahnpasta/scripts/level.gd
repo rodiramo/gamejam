c97e3ada118965a7dd1b50195cabe457db5471e2
Leif Haberecht <deskilling@tuta.com>
2026-09-26 01:34:53 +0200

    add MusikPlayer

--- zahnpasta/scripts/level.gd
+++ zahnpasta/scripts/level.gd
@@ -1,6 +1,8 @@
 class_name HUD
 extends Node2D
 
+signal switch_audio(a: int, b: int)
+
 enum State{
 	RUNNING,
 	PAUSED
@@ -12,10 +14,8 @@ enum State{
 
 var _state: State = State.PAUSED
 
-
 func _ready() -> void:
 	self.rythm_manager.initialize(config)
-
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("resume") and _state == State.PAUSED:
@@ -24,3 +24,10 @@ func _process(_delta: float) -> void:
 	elif Input.is_action_just_pressed("pause") and _state == State.RUNNING:
 		self.rythm_manager.stop()
 		_state = State.PAUSED
+
+
+var max_intensity := 5
+var intensity := 0
+
+func _switch_audio() -> void:
+	switch_audio.emit(intensity, max_intensity)
