f66f6fcb51237606a5ae0c1e68f26b6d7cb9221f
Marten Jasper <marten.jasper@innogames.com>
2026-09-26 00:55:07 +0200

    fix crashing and add feedback if movement was successful

--- zahnpasta/scripts/grid.gd
+++ zahnpasta/scripts/grid.gd
@@ -1,3 +1,4 @@
+class_name Grid
 extends Node2D
 
 @export var width = 48
@@ -9,12 +10,16 @@ extends Node2D
 
 var random = RandomNumberGenerator.new()
 
-func grid_pos_to_coordinats(grid_pos: Vector2) -> Vector2:
+func grid_pos_to_coordinats(grid_pos: Vector2):
 	return grid_zero_marker.global_position + (grid_pos * Vector2(width, height))
 
 func in_grid(grid_pos: Vector2) -> bool:
 	return grid_pos.x >= 0 and grid_pos.x < cells_x and grid_pos.y >= 0 and grid_pos.y < cells_y
 
-func move_to_grid(grid_pos: Vector2, node: Node2D):
+## takes the desired grid pos as Vectro2 and the Node2D to move. Returns a bool of how successful the movement was.
+func move_to_grid(grid_pos: Vector2, node: Node2D) -> bool: 
 	if in_grid(grid_pos):
 		node.global_position = grid_pos_to_coordinats(grid_pos)
+		return true
+	else:
+		return false
