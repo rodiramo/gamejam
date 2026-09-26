6737308c4916668727306f24b069c2161e307d5e
Hannes Olszewski <kontakt@hannes-olszewski.dev>
2026-09-26 11:05:25 +0200

    Add combo multiplier to score manager

--- zahnpasta/scripts/score_manager.gd
+++ zahnpasta/scripts/score_manager.gd
@@ -7,11 +7,14 @@ signal score_updated(score: int)
 @export var base_score_per_beat: int = 10
 @export var base_score_multiplier: float = 1.0
 @export var extra_score_multiplier_per_intensity_level: float = 0.1
+@export var extra_score_multiplier_per_combo_level: float = 0.01
 @export var rythm_manager: RythmManager
 
 
 var _score: int = 0
 var _multiplier: float
+var _intensity_multiplier: float = 0.0
+var _combo_multiplier: float = 0.0
 
 
 func _ready() -> void:
@@ -20,7 +23,17 @@ func _ready() -> void:
 
 
 func update_intensity(intensity: int) -> void:
-	_multiplier = base_score_multiplier + intensity * extra_score_multiplier_per_intensity_level
+	_intensity_multiplier = intensity * extra_score_multiplier_per_intensity_level
+	_update_multiplier()
+
+
+func update_combo(combo: int) -> void:
+	_combo_multiplier = combo * extra_score_multiplier_per_combo_level
+	_update_multiplier()
+
+
+func _update_multiplier() -> void:
+	_multiplier = base_score_multiplier + _intensity_multiplier + _combo_multiplier
 
 
 func _on_rythm_manager_beat_hit(_beat: int) -> void:
