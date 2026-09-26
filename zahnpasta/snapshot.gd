1ef31e72fdda605024f5d7eb4be2c109dace9993
TheRedCoder404 <jm.jasper05@gmx.de>
2026-09-26 15:38:32 +0200

    add movement tracker, track player up down movement

---
+++ zahnpasta/snapshot.gd
@@ -0,0 +1,23 @@
+class_name Snapshot
+extends Resource
+
+enum MoveType {
+	UP,
+	DOWN
+}
+
+var move_type: MoveType
+var beat: int
+var in_between_time: float
+
+
+func set_move_type(type: MoveType):
+	move_type = type
+
+
+func set_beats(on_beat: int):
+	beat = on_beat
+
+
+func set_in_between_time(time: float):
+	in_between_time = time
